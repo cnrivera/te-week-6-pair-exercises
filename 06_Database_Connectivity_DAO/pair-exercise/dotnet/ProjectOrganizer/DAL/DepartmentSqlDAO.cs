3d91a30ff5c2dd1c10d203dd995dd7975f04dbc5
Christina Rivera <cnrivera@gmail.com>
2020-02-17 15:50:18 -0500

    Pair Project week 6

--- 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/DepartmentSqlDAO.cs
+++ 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/DepartmentSqlDAO.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectOrganizer.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,40 @@ namespace ProjectOrganizer.DAL
         /// <returns></returns>
         public IList<Department> GetDepartments()
         {
-            throw new NotImplementedException();
+            List<Department> departments = new List<Department>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM department", conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Department dept = new Department();
+
+                        dept.Id = Convert.ToInt32(reader["department_id"]);
+                        dept.Name = Convert.ToString(reader["name"]);
+
+                        departments.Add(dept);
+                    }
+                }
+            }
+
+                
+            catch (SqlException ex)
+            {
+
+                Console.WriteLine("Error getting department");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+
+            return departments;
+
+
         }
 
         /// <summary>
@@ -33,9 +67,33 @@ namespace ProjectOrganizer.DAL
         /// <returns>The id of the new department (if successful).</returns>
         public int CreateDepartment(Department newDepartment)
         {
-            throw new NotImplementedException();
+            int id = 0;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO department (name) VALUES(@name);", conn);
+                    cmd.Parameters.AddWithValue("@name", newDepartment.Name);
+
+                    cmd.ExecuteNonQuery();
+
+                    cmd = new SqlCommand("SELECT MAX(id) from department", conn);
+                    id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error adding department");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+            return id;
         }
-        
+
+
+
         /// <summary>
         /// Updates an existing department.
         /// </summary>
@@ -43,7 +101,34 @@ namespace ProjectOrganizer.DAL
         /// <returns>True, if successful.</returns>
         public bool UpdateDepartment(Department updatedDepartment)
         {
-            throw new NotImplementedException();
+            bool successful = false;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE department (name) SET(@name) WHERE (@id);", conn);
+                    cmd.Parameters.AddWithValue("@name", updatedDepartment.Name);
+
+                   int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                    {
+                        successful = true;
+                    }
+                        
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error updating department");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+            return successful;
+            
         }
 
     }
