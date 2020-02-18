8a01ebb3b13f390ffdb58a61577db09415f0c610
Kim Westendorf <ktwestendorf@gmail.com>
2020-02-18 14:43:56 -0500

    Pair exercises DAO Part 1

--- 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/EmployeeSqlDAO.cs
+++ 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/EmployeeSqlDAO.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectOrganizer.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,42 @@ namespace ProjectOrganizer.DAL
         /// <returns>A list of all employees.</returns>
         public IList<Employee> GetAllEmployees()
         {
-            throw new NotImplementedException();
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM employee", conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Employee empl = new Employee();
+
+                        empl.EmployeeId = Convert.ToInt32(reader["employee_ID"]);
+                        empl.FirstName = Convert.ToString(reader["first_name"]);
+                        empl.LastName = Convert.ToString(reader["last_name"]);
+                        empl.JobTitle = Convert.ToString(reader["job_title"]);
+                        empl.Gender = Convert.ToString(reader["gender"]);
+                        empl.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+
+                        employees.Add(empl);
+                    }
+                }
+            }
+
+
+            catch (SqlException ex)
+            {
+
+                Console.WriteLine("Error getting employee list");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+
+            return employees;
         }
 
         /// <summary>
@@ -35,7 +71,44 @@ namespace ProjectOrganizer.DAL
         /// <returns>A list of employees that match the search.</returns>
         public IList<Employee> Search(string firstname, string lastname)
         {
-            throw new NotImplementedException();
+            List<Employee> employeeSearch = new List<Employee>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * from employee WHERE first_name LIKE @first_name AND last_name LIKE @last_name", conn);
+                    cmd.Parameters.AddWithValue("@first_name", "%" + firstname + "%");
+                    cmd.Parameters.AddWithValue("@last_name", "%" + lastname + "%");
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Employee empl = new Employee();
+
+                        empl.EmployeeId = Convert.ToInt32(reader["employee_ID"]);
+                        empl.FirstName = Convert.ToString(reader["first_name"]);
+                        empl.LastName = Convert.ToString(reader["last_name"]);
+                        empl.JobTitle = Convert.ToString(reader["job_title"]);
+                        empl.Gender = Convert.ToString(reader["gender"]);
+                        empl.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+
+                        employeeSearch.Add(empl);
+                    }
+
+                    return employeeSearch;
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error finding employee");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+            return employeeSearch ;
         }
 
         /// <summary>
@@ -44,7 +117,42 @@ namespace ProjectOrganizer.DAL
         /// <returns></returns>
         public IList<Employee> GetEmployeesWithoutProjects()
         {
-            throw new NotImplementedException();
+            List<Employee> employeeNoProject = new List<Employee>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM employee LEFT JOIN project_employee ON project_employee.employee_id = employee.employee_id WHERE project_id IS NULL", conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Employee emplnp = new Employee();
+
+                        emplnp.EmployeeId = Convert.ToInt32(reader["employee_ID"]);
+                        emplnp.FirstName = Convert.ToString(reader["first_name"]);
+                        emplnp.LastName = Convert.ToString(reader["last_name"]);
+                        emplnp.JobTitle = Convert.ToString(reader["job_title"]);
+                        emplnp.Gender = Convert.ToString(reader["gender"]);
+                        emplnp.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+
+                        employeeNoProject.Add(emplnp);
+                    }
+                }
+            }
+
+
+            catch (SqlException ex)
+            {
+
+                Console.WriteLine("Error getting employee list");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+
+            return employeeNoProject;
         }
     }
 }
--- 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/ProjectSqlDAO.cs
+++ 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/ProjectSqlDAO.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectOrganizer.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,40 @@ namespace ProjectOrganizer.DAL
         /// <returns></returns>
         public IList<Project> GetAllProjects()
         {
-            throw new NotImplementedException();
+            List<Project> projects = new List<Project>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM project", conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Project proj = new Project();
+
+                        proj.ProjectId = Convert.ToInt32(reader["project_id"]);
+                        proj.Name = Convert.ToString(reader["name"]);
+                        proj.StartDate = Convert.ToDateTime(reader["from_date"]);
+                        proj.EndDate = Convert.ToDateTime(reader["to_date"]);
+
+                        projects.Add(proj);
+                    }
+                }
+            }
+
+
+            catch (SqlException ex)
+            {
+
+                Console.WriteLine("Error getting project list");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+
+            return projects;
         }
 
         /// <summary>
@@ -34,7 +68,34 @@ namespace ProjectOrganizer.DAL
         /// <returns>If it was successful.</returns>
         public bool AssignEmployeeToProject(int projectId, int employeeId)
         {
-            throw new NotImplementedException();
+            bool successful = false;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO project_employee (project_id, employee_id) VALUES (@project_id, @employee_id)", conn);
+                    cmd.Parameters.AddWithValue("@project_id", projectId);
+                    cmd.Parameters.AddWithValue("@employee_id", employeeId);
+
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                    {
+                        successful = true;
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error assigning employee to project");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+            return successful;
         }
 
         /// <summary>
@@ -45,7 +106,34 @@ namespace ProjectOrganizer.DAL
         /// <returns>If it was successful.</returns>
         public bool RemoveEmployeeFromProject(int projectId, int employeeId)
         {
-            throw new NotImplementedException();
+            bool successful = false;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM project_employee WHERE project_id = @project_id AND employee_id = @employee_id", conn);
+                    cmd.Parameters.AddWithValue("@project_id", projectId);
+                    cmd.Parameters.AddWithValue("@employee_id", employeeId);
+
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                    {
+                        successful = true;
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error removing employee from project");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+            return successful;
         }
 
         /// <summary>
@@ -55,7 +143,31 @@ namespace ProjectOrganizer.DAL
         /// <returns>The new id of the project.</returns>
         public int CreateProject(Project newProject)
         {
-            throw new NotImplementedException();
+            int id = 0;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO project (name, from_date, to_date) VALUES(@name, @from_date, @to_date);", conn);
+                    cmd.Parameters.AddWithValue("@name", newProject.Name);
+                    cmd.Parameters.AddWithValue("@from_date", newProject.StartDate);
+                    cmd.Parameters.AddWithValue("@to_date", newProject.EndDate);
+
+                    cmd.ExecuteNonQuery();
+
+                    cmd = new SqlCommand("SELECT MAX(project_id) from project", conn);
+                    id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error adding project");
+                Console.WriteLine("The error was: " + ex.Message);
+            }
+            return id;
         }
 
     }
